05fbac745aaaa505c05e3680b00840f6e690e852
lee hyunwoo <dlgusdn978@naver.com>
2023-10-25 15:56:48 +0900

    feat(frontend):init settings

--- workspace/bookulove-frontend/app/src/main/java/com/example/frontend/ui/components/ChatInfo.kt
+++ workspace/bookulove-frontend/app/src/main/java/com/example/frontend/ui/components/ChatInfo.kt
@@ -1,8 +1,6 @@
 package com.example.frontend.ui.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,15 +11,12 @@ import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.frontend.R
 
 @Preview
 @Composable
--- workspace/bookulove-frontend/app/src/main/java/com/example/frontend/ui/screens/user/Chat.kt
+++ workspace/bookulove-frontend/app/src/main/java/com/example/frontend/ui/screens/user/Chat.kt
@@ -2,11 +2,11 @@ package com.example.frontend.ui.screens.user
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.frontend.ui.components.ChatInfo
+
 
 @Composable
 fun Chat(){
