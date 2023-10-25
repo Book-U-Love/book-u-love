b3a5e121ac48bf3e12747c77bd8039e1b7e19bc7
lee hyunwoo <dlgusdn978@naver.com>
2023-10-25 09:21:48 +0900

    [FE] Init : frontend init settings

---
+++ workspace/bookulove-frontend/app/src/main/java/com/example/frontend/ui/screens/book/BookSearch.kt
@@ -0,0 +1,12 @@
+package com.example.frontend.ui.screens.book
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BookSearch(){
+    Box {
+        Text("bookSearchPage")
+    }
+}
