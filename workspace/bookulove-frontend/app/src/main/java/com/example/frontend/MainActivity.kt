b78b15bd5919a2a9acd8934f650da509f033f3cc
mji4209 <mji4209@naver.com>
2023-10-30 10:26:17 +0900

    add(frontend) : 중간 주석 제거

--- workspace/bookulove-frontend/app/src/main/java/com/example/frontend/MainActivity.kt
+++ workspace/bookulove-frontend/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -89,19 +89,10 @@ class MainActivity : ComponentActivity() {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return
         }
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location : Location? ->
-                // Got last known location. In some rare situations this can be null.
-
                 if (location != null) {
                     locInfo = LatLng(location.latitude, location.longitude)
                 }
