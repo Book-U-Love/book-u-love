6dbfd0d94b77d3b2a1672700dba6150a5626bbcd
유승민 <byuri1356@gmail.com>
2023-11-01 15:51:58 +0900

    feat(backend): implement apigateway uri

--- workspace/bookulove-backend/apigateway-service/src/main/java/org/bookulove/apigateway/filter/GlobalFilter.java
+++ workspace/bookulove-backend/apigateway-service/src/main/java/org/bookulove/apigateway/filter/GlobalFilter.java
@@ -1,5 +1,6 @@
 package org.bookulove.apigateway.filter;
 
+
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
@@ -9,41 +10,41 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @Component
+@Slf4j
 public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-
-    @Data
-    public static class Config {
-        private String message;
-        private boolean showPreLogger;
-        private boolean showPostLogger;
-    }
 
     public GlobalFilter() {
         super(Config.class);
     }
 
-    @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            ServerHttpResponse response = exchange.getResponse();
-            log.info("Global Filter Message: {}", config.getMessage());
-
-            // Global pre Filter
-            if (config.isShowPreLogger()) {
-                log.info("Global Filter Start: request uri -> {}", request.getLocalAddress());
-            }
-
-            // Global Post Filter
-            //Mono는 webflux에서 단일값 전송할때 Mono값으로 전송
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                if (config.isShowPostLogger()) {
-                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
-                }
-            }));
-        };
+    @Data
+    public static class Config{
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Global PRE baseMessage : {}", config.getBaseMessage());
+
+            if(config.isPreLogger()){
+                System.out.println("============================");
+                log.info("Global Filter Start : request id = {}", request.getId());
+            }
+
+
+            //custom post filter
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if(config.isPostLogger()){
+                    log.info("Global Filter End : response code = {}", response.getStatusCode());
+                }
+            }));
+        });
+    }
 }
--- workspace/bookulove-backend/auth-service/src/main/java/org/bookyoulove/auth/AuthConfig.java
+++
@@ -1,9 +0,0 @@
-package org.bookyoulove.auth;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("org.bookyoulove.common")
-public class AuthConfig {
-}
