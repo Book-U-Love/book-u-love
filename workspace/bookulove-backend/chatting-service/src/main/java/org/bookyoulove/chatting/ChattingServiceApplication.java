0feac29db9aa99c973d6b8f4c2ca0c34b40ccd7c
유승민 <byuri1356@gmail.com>
2023-11-07 10:19:54 +0900

    init(backend): init chatting-service

---
+++ workspace/bookulove-backend/chatting-service/src/main/java/org/bookyoulove/chatting/ChattingServiceApplication.java
@@ -0,0 +1,12 @@
+package org.bookyoulove.chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingServiceApplication.class, args);
+    }
+}
---
+++ workspace/bookulove-backend/chatting-service/src/main/java/org/bookyoulove/chatting/global/config/ChattingConfig.java
@@ -0,0 +1,9 @@
+package org.bookyoulove.chatting.global.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.bookulove.common")
+public class ChattingConfig {
+}
