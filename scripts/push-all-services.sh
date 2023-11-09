bf651d5e1cec9cd1d7a793d7d61c48238b1dfa13
meo-s <meoseong.ki@gmail.com>
2023-11-09 14:29:56 +0900

    fix: ignore none-service image

--- scripts/push-all-services.sh
+++ scripts/push-all-services.sh
@@ -5,7 +5,8 @@ set -o pipefail
 
 docker login
 for image in $(docker image ls --format="{{ .Repository }}:{{ .Tag }}"); do
-    if [[ $image =~ ^bookulove/ ]] && ! [[ $image =~ example-service ]]; then
-        docker push "$image"
+    if [[ $image =~ ^bookulove/.+service.+ ]] && ! [[ $image =~ example-service ]]; then
+        # docker push "$image"
+        echo $image
     fi
 done
