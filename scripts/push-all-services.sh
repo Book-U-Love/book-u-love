9d06b23855cd7bfb1c325353ac2be995d4c845ec
meo-s <meoseong.ki@gmail.com>
2023-11-09 14:32:34 +0900

    fix: strictly check image tag

--- scripts/push-all-services.sh
+++ scripts/push-all-services.sh
@@ -5,8 +5,7 @@ set -o pipefail
 
 docker login
 for image in $(docker image ls --format="{{ .Repository }}:{{ .Tag }}"); do
-    if [[ $image =~ ^bookulove/.+service.+ ]] && ! [[ $image =~ example-service ]]; then
-        # docker push "$image"
-        echo $image
+    if [[ $image =~ ^bookulove/.+service:[a-z0-9.]+ ]] && ! [[ $image =~ example-service ]]; then
+        docker push "$image"
     fi
 done
